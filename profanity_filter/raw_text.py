a0c082ed3d21ed97eddc737b7de9937932537e9c
Timur Iskhakov <iskhakovt@gmail.com>
2015-05-28 17:49:42 +0300

    RawText.__levenshtein_distance is deleted

--- profanity_filter/raw_text.py
+++ profanity_filter/raw_text.py
@@ -27,31 +27,6 @@ class RawText:
         self.morph = pymorphy2.MorphAnalyzer()
         self.stops = ('.', '!', '?', '…')
 
-    @staticmethod
-    def __levenshtein_distance(a, b):
-        """Calculates the Levenshtein distance.
-
-        :param a: :class:`str` first string
-        :param b: :class:`str` second string
-        :return: Levenshtein distance
-        """
-
-        n, m = len(a), len(b)
-        if n > m:
-            a, b = b, a
-            n, m = m, n
-
-        current_row = range(n + 1)
-        for i in range(1, m + 1):
-            previous_row, current_row = current_row, [i] + [0] * n
-            for j in range(1, n + 1):
-                add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
-                if a[j - 1] != b[i - 1]:
-                    change += 1
-                current_row[j] = min(add, delete, change)
-
-        return current_row[n]
-
     def __normalize(self, token):
         # Yeah, this is fun
         if token[:4] == 'пидо':

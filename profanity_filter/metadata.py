3dd68de514893cbc027e19d4f57e7bfd589fa1b3
Timur Iskhakov <iskhakovt@gmail.com>
2015-05-29 10:41:56 +0300

    Version 0.2

--- profanity_filter/metadata.py
+++ profanity_filter/metadata.py
@@ -9,7 +9,7 @@ Information describing the project.
 
 package = 'profanity_filter'
 project = "profanity-filter"
-version = '0.1'
+version = '0.2'
 description = 'Cleaning the text of profanity'
 authors = ['Timur Iskhakov']
 authors_string = ', '.join(authors)
